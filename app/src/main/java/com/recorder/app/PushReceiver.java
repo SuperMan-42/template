4fde3943525591951190605249bf395f59d81c43
hpw <424346976@qq.com>
2017-12-09 17:33:58 +0800

    完成升级所有功能

--- app/src/main/java/com/recorder/app/PushReceiver.java
+++ app/src/main/java/com/recorder/app/PushReceiver.java
@@ -1,10 +1,32 @@
 package com.recorder.app;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.baidu.android.pushservice.PushMessageReceiver;
+import com.core.utils.Constants;
+import com.core.utils.CoreUtils;
+import com.core.utils.DeviceUtils;
+import com.google.gson.Gson;
+import com.orhanobut.logger.Logger;
+import com.recorder.R;
+import com.recorder.mvp.model.api.Api;
+import com.recorder.mvp.model.entity.AppVersionBean;
+import com.recorder.utils.CommonUtils;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.vector.update_app.HttpManager;
+import com.vector.update_app.UpdateAppBean;
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.UpdateCallback;
 
 import java.util.List;
+import java.util.Map;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 /**
  * Created by hpw on 17-12-6.
@@ -13,41 +35,99 @@ import java.util.List;
 public class PushReceiver extends PushMessageReceiver {
     @Override
     public void onBind(Context context, int i, String s, String s1, String s2, String s3) {
-
     }
 
     @Override
     public void onUnbind(Context context, int i, String s) {
-
     }
 
     @Override
     public void onSetTags(Context context, int i, List<String> list, List<String> list1, String s) {
-
     }
 
     @Override
     public void onDelTags(Context context, int i, List<String> list, List<String> list1, String s) {
-
     }
 
     @Override
     public void onListTags(Context context, int i, List<String> list, String s) {
-
     }
 
     @Override
     public void onMessage(Context context, String s, String s1) {
-
     }
 
     @Override
     public void onNotificationClicked(Context context, String s, String s1, String s2) {
-
     }
 
     @Override
-    public void onNotificationArrived(Context context, String s, String s1, String s2) {
+    public void onNotificationArrived(Context context, String s, String s1, String s2) {//s=title s1=content
+        Logger.d("push=> " + s + s1);
+        Activity activity = CoreUtils.obtainAppComponentFromContext(context).appManager().getCurrentActivity();
+        new UpdateAppManager.Builder()
+                .setActivity(activity)
+                .setHttpManager(new HttpManager() {
+                    @Override
+                    public void asyncGet(@NonNull String url, @NonNull Map<String, String> params, @NonNull HttpManager.Callback callBack) {
+                        callBack.onResponse(s1);
+                    }
 
+                    @Override
+                    public void asyncPost(@NonNull String url, @NonNull Map<String, String> params, @NonNull Callback callBack) {
+
+                    }
+
+                    @Override
+                    public void download(@NonNull String url, @NonNull String path, @NonNull String fileName, @NonNull FileCallback callback) {
+                        new RxPermissions(activity)
+                                .request(WRITE_EXTERNAL_STORAGE)
+                                .doOnNext(granted -> {
+                                    if (!granted) {
+                                        throw new RuntimeException("no permission");
+                                    }
+                                })
+                                .compose(CommonUtils.transformService(context, url, path + fileName, false, true, callback))
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(baseDownloadTask -> Logger.d("path" + path + fileName + "download=> " + baseDownloadTask.getSoFarBytes() + " " + baseDownloadTask.getTotalBytes() + " progress=> " + ((float) baseDownloadTask.getSoFarBytes()) / baseDownloadTask.getTotalBytes()));
+                    }
+                })
+                .setUpdateUrl(Api.APP_DOMAIN + "app/version")
+                .hideDialogOnDownloading(false)
+                .setTopPic(R.mipmap.top_4)
+                .setThemeColor(CoreUtils.getColor(activity, R.color.colorStatus))
+                .setTargetPath(Constants.SDCARD_PATH)
+                .showIgnoreVersion()
+                .build()
+                .checkNewApp(new UpdateCallback() {
+                    @Override
+                    protected UpdateAppBean parseJson(String json) {
+                        AppVersionBean.DataEntity.VersionInfoEntity versionInfo = new Gson().fromJson(json, AppVersionBean.class).getData().getVersion_info();
+                        List<String> list = versionInfo.getDes();
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (String string : list) {
+                            stringBuilder.append(string).append("\r\n");
+                        }
+                        stringBuilder.delete(stringBuilder.length() - 5, stringBuilder.length() - 1);
+                        UpdateAppBean updateAppBean = new UpdateAppBean();
+                        updateAppBean
+                                //（必须）是否更新Yes,No
+                                .setUpdate(!DeviceUtils.getVersionName(activity).equals(versionInfo.getNew_version()) ? "Yes" : "No")
+                                //（必须）新版本号，
+                                .setNewVersion(versionInfo.getNew_version())
+                                //（必须）下载地
+                                .setApkFileUrl("https://raw.githubusercontent.com/SuperMan42/template/haoxiang/app/app-debug.apk")
+                                //（必须）更新内容
+                                .setUpdateLog(stringBuilder.toString())
+                                //大小，不设置不显示大小，可以不设置
+//                                .setTargetSize(String.valueOf(size))
+                                //是否强制更新，可以不设置
+                                .setConstraint(versionInfo.getIs_force() == 1);
+                        //设置md5，可以不设置
+//                                .setNewMd5(jsonObject.optString("new_md51"));
+                        return updateAppBean;
+                    }
+                });
     }
 }
